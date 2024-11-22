1d086ad951d38b0b3f455ea717d1d0045688b3fc
ChanceDaWrapper <triplemychances@gmail.com>
2024-11-21 21:42:56 -0800

    Update to settings

--- django_fantasy/settings.py
+++ django_fantasy/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = "django_fantasy.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),  # Use /tmp for write access
     }
 }
 
